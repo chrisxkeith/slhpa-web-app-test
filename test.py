138958c7d7f81814af8b2708458d16d7941ac4f2
CK <chris.keith@gmail.com>
2019-04-12 08:56:14 -0700

    Add multiple browser support.

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import ElementNotSelectableException
 # TODO : from django.test import TestCase
 import time
 import os
+import platform
 import sys
 import datetime
 import re
@@ -17,6 +18,11 @@ import re
 def log(message):
     script_name = sys.argv[0]
     print(str(datetime.datetime.now()) + '\t' + script_name + '\t' + message)
+
+
+def log_exception(message):
+    log(message + ' : ' + str(sys.exc_info()
+                              [0]) + ' : ' + str(sys.exc_info()[1]))
 
 
 class Tester:
@@ -28,26 +34,44 @@ class Tester:
         time.sleep(2)
         self.driver.close()
 
-
     def main(self):
         starttime = datetime.datetime.now()
-        log("Started test")
-        try:
-            self.driver = webdriver.Chrome()
-        except:
-            self.driver = webdriver.Firefox()
+        log("Started tests")
+        browser_types = ["Chrome", "Firefox"]
+        if platform.system() == 'Windows':
+            browser_types.append("Edge")
+        if platform.system() == 'Darwin':
+            browser_types.append("Safari")
+        for browser_type in browser_types:
+            try:
+                if browser_type == 'Chrome':
+                    self.driver = webdriver.Chrome()
+                else:
+                    if browser_type == 'Firefox':
+                        self.driver = webdriver.Firefox()
+                    else:
+                        if browser_type == 'Edge':
+                            self.driver = webdriver.Edge()
+                        else:
+                            if browser_type == 'Safari':
+                                self.driver = webdriver.Safari()
+                            else:
+                                raise "Unknown browser_type: " + browser_type
 
-        self.wait = WebDriverWait(self.driver, 10, poll_frequency=1,
-                                    ignored_exceptions=[NoSuchElementException,
-                                                        ElementNotVisibleException,
-                                                        ElementNotSelectableException])
+                self.wait = WebDriverWait(self.driver, 10, poll_frequency=1,
+                                          ignored_exceptions=[NoSuchElementException,
+                                                              ElementNotVisibleException,
+                                                              ElementNotSelectableException])
 
-        # Implicit wait - tells web driver to poll the DOM for specified time;
-        # wait is set for duration of web driver object.
-        self.driver.implicitly_wait(2)
+                # Implicit wait - tells web driver to poll the DOM for specified time;
+                # wait is set for duration of web driver object.
+                self.driver.implicitly_wait(2)
 
-        # self.run_test('http://127.0.0.1/slhpa/')
-        self.run_test('https://slhpa-03.appspot.com/slhpa/')
+                # self.run_test('http://127.0.0.1/slhpa/')
+                self.run_test('https://slhpa-03.appspot.com/slhpa/')
+            except:
+                log_exception('Failure in browser: ' + browser_type)
+
         seconds = (datetime.datetime.now() - starttime).seconds
         log("Elapsed seconds: " + str(int(seconds)))
 
